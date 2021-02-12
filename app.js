4ca893559d82bb2e439f9d9a91b603cd01133e0e
Neydor Avila <navila@fivetech.co>
2021-02-11 23:54:25 -0500

    Back con cifrado y jwt para login funcional

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
 //ejecutar con nodemon de ser posible
 
-const mysql = require('mysql');
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require("express-session");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const jwt = require('jsonwebtoken');
+const { response } = require('express');
 //en caso de desplegarlo
 const PORT = process.env.PORT || 3128;
+
 const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: ["http://localhost:3128"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-
+app.use(
+    session({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 06 * 24,
+        },
+    })
+);
 
 //--------------- Conexion con la BD -----------------------------------------
 const conexion = mysql.createConnection({
@@ -25,40 +62,108 @@ conexion.connect(function(error) {
     if (error) { throw error; } else { console.log('Conexion establecida'); }
 
 });
+// {
+//     "Nombre_Usuario":"Neydor",
+//     "Contrasenia":"qwerty",
+//     "Documento":"111222333",
+//     "Correo":"neydor.avila@gmail.com",
+//     "Sexo":1,
+//     "Telefono":"2242424",
+//     "Residencia":"null",
+//     "Tipo_Documento_ID":1,
+//     "Nacionalidad_ID":2
+// }
+
+app.post("/registrar", (req, res) => {
+    const username = req.body.Nombre_Usuario;
+    const password = req.body.Contrasenia;
+    const Documento = req.body.Documento;
+    const Correo = req.body.Correo;
+    const Sexo = req.body.Sexo;
+    const Telefono = req.body.Telefono;
+    const Residencia = req.body.Residencia;
+    const Tipo_Documento_ID = req.body.Tipo_Documento_ID;
+    const Nacionalidad_ID = req.body.Nacionalidad_ID;
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+        }
+        conexion.query("INSERT INTO Usuarios (Nombre_Usuario,Documento,Correo,Contrasenia,Sexo,Telefono,Residencia,Tipo_Documento_ID,Nacionalidad_ID) VALUES (?,?,?,?,?,?,?,?,?)", [username, Documento, Correo, hash, Sexo, Telefono, Residencia, Tipo_Documento_ID, Nacionalidad_ID],
+            (err, result) => {
+                console.log(err);
+            });
+    });
+});
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers["x-access-token"];
+
+    if (!token) {
+        res.send(":( se necesita token")
+    } else {
+        jwt.verify(token, "jwtSecret", (err, decoded) => {
+            if (err) {
+                res.json({ auth: false, message: "Fallo la autentificacion" });
+            } else {
+                req.userId = decoded.id;
+                next();
+            }
+        })
+    }
+}
+
+app.get('/isUserAuth', verifyJWT, (req, res) => {
+    res.send("Autenticado");
+})
+
+app.get("/login", (req, res) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {
+        res.send({ loggedIn: false });
+    }
+});
 
 app.post("/login", (req, res) => {
-    const username = req.body.username;
+    const Nombre_Usuario = req.body.username;
     const password = req.body.password;
-    const sql = `SELECT * FROM Usuarios WHERE Nombre_Usuario = ${username}`;
-    conexion.query(sql,
-        function(error, results, fields) {
-            if (error)
-                throw error;
-            if (results.length > 0) {
-                if (sql.Contrasenia === password) {
-                    const payload = {
-                        check: true
-                    };
-                    const token = jwt.sign(payload, app.get('llave'), {
-                        expiresIn: 1440
-                    });
-                    res.json({
-                        mensaje: 'Autentificacion correcta',
-                        token: token
-                    });
-                } else {
-                    res.json({ mensaje: "Usuario o contrasena incorrectos." })
-                }
+    console.log(typeof ID_Usuario);
+    const sql = `SELECT * FROM Usuarios WHERE Nombre_Usuario = "${Nombre_Usuario}"`;
+    conexion.query(
+        sql,
+
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].Contrasenia, (error, response) => {
+                    if (response) {
+                        const id = result[0].id;
+                        const token = jwt.sign({ id }, "jwtSecret", {
+                            expiresIn: 300,
+                        });
+                        req.session.user = result;
+                        console.log(result);
+                        res.json({ auth: true, token: token, result: result })
+                    } else {
+                        res.json({
+                            auth: false,
+                            message: "Usuario o contraseña incorrecta.",
+                        });
+                    }
+                });
             } else {
-                res.json({ mensaje: "Usuario o contrasena incorrectos." })
+                res.json({ auth: false, result: result });
             }
         });
 });
+
 //---------------------------------------------------------   crud ESTACIONES  ___________________________________________________________________________
 
 
 // traer todas las estaciones
-app.get('/estaciones', verifyToken, (req, res) => {
+app.get('/estaciones', (req, res) => {
     jwt.verify(req.token, 'secretkey', (error, authData) => {
         if (error) {
             res.sendStatus(403);
@@ -79,17 +184,17 @@ app.get('/estaciones', verifyToken, (req, res) => {
 });
 
 // Authorization: Bearer <token>
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
+// function verifyToken(req, res, next) {
+//     const bearerHeader = req.headers['authorization'];
 
-    if (typeof bearerHeader !== 'undefined') {
-        const bearerToken = bearerHeader.split(" ")[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
+//     if (typeof bearerHeader !== 'undefined') {
+//         const bearerToken = bearerHeader.split(" ")[1];
+//         req.token = bearerToken;
+//         next();
+//     } else {
+//         res.sendStatus(403);
+//     }
+// }
 
 // traer una estacion segun el id
 app.get('/estaciones/:estacion_id', (req, res) => {
@@ -201,7 +306,7 @@ app.get('/usuarios', (req, res) => {
 // traer un usuario segun el id
 app.get('/usuarios/:ID_Usuario', (req, res) => {
 
-    const { ID_Usuario } = req.params
+    const { ID_Usuario } = req.params;
     const sql = `SELECT * FROM Usuarios WHERE ID_Usuario = ${ID_Usuario}`;
 
     conexion.query(sql, function(error, results, fields) {
@@ -217,6 +322,7 @@ app.get('/usuarios/:ID_Usuario', (req, res) => {
     });
 
 });
+
 // crear un usuario
 app.post('/usuarios', (req, res, netx) => {
 
